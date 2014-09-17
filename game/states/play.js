d3ddb5264c4c7574bce0ec5d93cb067b7b30166a
Mark Nachazel <marknach@vt.edu>
2014-09-17 16:11:38 -0400

    unit walks path

--- game/states/play.js
+++ game/states/play.js
@@ -5,6 +5,10 @@
     create: function() {
       this.game.stage.backgroundColor = '#FFF';
       this.sprite = this.game.add.sprite(0, 0, 'map');
+      this.unit = this.game.add.sprite(0, 435, 'unit');
+      this.tween = this.game.add.tween(this.unit).to({x: 695}, 1000)
+                                                 .to({y: 100}, 1000)
+                                                 .start();
     },
     update: function() {
 
