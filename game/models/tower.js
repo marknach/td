0fb85d772d9f35bd4240a3c68988e3e8481e9316
Mark Nachazel <marknach@vt.edu>
2014-09-17 17:39:56 -0400

    add function to create 10x units over 5 seconds and walk the path

---
+++ game/models/tower.js
@@ -0,0 +1,5 @@
+function Tower() {}
+Tower.prototype = {
+    shoot: function() {
+    }
+}
--- game/states/play.js
+++ game/states/play.js
@@ -5,16 +5,24 @@
     create: function() {
       this.game.stage.backgroundColor = '#FFF';
       this.sprite = this.game.add.sprite(0, 0, 'map');
-      this.unit = this.game.add.sprite(0, 435, 'unit');
-      this.walkPath(this.unit);
+      this.spawnLevel();
     },
     update: function() {
 
     },
     walkPath: function(obj) {
-      this.tween = this.game.add.tween(this.unit).to({x: 695}, 1000)
-                                                 .to({y: 100}, 1000)
+      this.tween = this.game.add.tween(this.unit).to({x: 695}, 5000)
+                                                 .to({y: 100}, 5000)
                                                  .start();
+    },
+    spawnLevel: function(unit) {
+      this.units = this.game.add.group();
+      this.game.time.events.repeat(Phaser.Timer.SECOND / 2, 10, this.spawnUnit, this);
+    },
+    spawnUnit: function() {
+      this.unit = this.game.add.sprite(0, 435, 'unit');
+      this.units.add(this.unit);
+      this.walkPath(this.unit);
     }
   };
   
