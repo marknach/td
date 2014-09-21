f007c9b65c8a75dcfdbafeae322c03b4fdd4098f
Mark Nachazel <marknach@vt.edu>
2014-09-21 17:19:30 -0400

    now shoots bullets that kill enemies

--- dist/js/game.js
+++ dist/js/game.js
@@ -108,12 +108,22 @@ module.exports = Menu;
       this.enemies.setAll('anchor.x', 0.5);
       this.enemies.setAll('anchor.y', 0.5);
 
+      this.bullets = this.game.add.group();
+      this.bullets.enableBody = true;
+      this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+      this.bullets.createMultiple(30, 'bullet');
+      this.bullets.setAll('anchor.x', 0.5);
+      this.bullets.setAll('anchor.y', 1);
+      this.bullets.setAll('outOfBoundsKill', true);
+      this.bullets.setAll('checkWorldBounds', true);
+
       this.player.addTower(new Tower(this.game, 500, 300));
       this.livesText = this.game.add.text(680, 550, 'lives: 20', { font: "20px Arial", fill: "#ffffff", align: "left" });
 
     },
     update: function() {
        this.fire();
+       this.game.physics.arcade.collide(this.bullets, this.enemies, this.bulletHitsEnemy, null, this);
        this.game.physics.arcade.overlap(this.castle, this.enemies, this.enemyReachedCastle, null, this);
     },
     walkPath: function(obj) {
@@ -140,6 +150,10 @@ module.exports = Menu;
       _enemy.kill();
       if ( this.player.lives === 0 )
           this.game.state.start('gameover');
+    },
+    bulletHitsEnemy: function(_bullet, _enemy) {
+      _bullet.kill();
+      _enemy.kill();
     }
   };
   
--- dist/js/models/tower.js
+++ dist/js/models/tower.js
@@ -2,16 +2,23 @@ function Tower(game, x, y) {
   this.sprite = game.add.sprite(x, y, 'tower');
   this.game = game;
   this.lastFired = game.time.now;
+  this.range = 300;
   this.fireRate = 1000;
 }
 Tower.prototype = {
     fire: function(_tower) {
-      enemy = _tower.game.state.states['play'].enemies.getFirstAlive();
+      var enemy = _tower.game.state.states['play'].enemies.getFirstAlive();
       if ( enemy && _tower.game.time.now - _tower.lastFired  >= _tower.fireRate ) {
-        console.log('pew pew');
-        enemy.kill();
+        var bullet = _tower.game.add.sprite(_tower.sprite.position.x, _tower.sprite.position.y, 'bullet');
+        _tower.game.physics.enable(bullet, Phaser.Physics.ARCADE);
+        _tower.game.physics.arcade.moveToObject(bullet, enemy, 600);
+        _tower.game.state.states['play'].bullets.add(bullet);
         _tower.lastFired = _tower.game.time.now;
       }
     },
+    getFirstInRange: function(_tower) {
+      var enemies = _tower.game.state.states['play'].enemies;
+      return this.iterate('alive', true, Phaser.Group.RETURN_CHILD);
+    }
 
 }
